e94b3a83d12cadeea22bd5054ec062076ee95813
Marmik Patel <44599895+Marmik2003@users.noreply.github.com>
2022-11-28 12:25:16 +0530

    Add files via upload

---
+++ src/components/About/AboutCard.js
@@ -0,0 +1,41 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hi Everyone, I am <span className="purple">Dharm Patel.</span>
+            from <span className="purple"> Bhubaneswar, India.</span>
+            <br />I am a junior pursuing IMSC in Maths and Computing in BIT
+            Mesra.
+            <br />
+            <br />
+            Apart from coding, some other activities that I love to do!
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Playing Chess
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Listening to Music
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Travelling
+            </li>
+          </ul>
+
+          <p style={{ color: "rgb(155 126 172)" }}>
+            "Work Hard Dream Big."{" "}
+          </p>
+          <footer className="blockquote-footer">Dharm Patel.</footer>
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
